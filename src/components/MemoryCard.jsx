1557bf7015aeeeda319c59ee3ece0e8d3d05bcd6
Lockett Pundt <kalifornium@hotmail.com>
2020-03-24 11:02:36 -0400

    component for the memory card.

---
+++ src/components/MemoryCard.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './MemoryCard.css'
+
+class MemoryCard extends Component {
+  render() {
+    return (
+      <div className="MemoryCard">
+        <img 
+        src='https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png' 
+        alt='digital crafts logo' 
+        />
+      </div>
+    );
+  };
+}
+
+export default MemoryCard;
