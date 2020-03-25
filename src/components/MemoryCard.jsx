8c8e8ebc2a051790b894c967c3b79c60e81f4855
Lockett Pundt <kalifornium@hotmail.com>
2020-03-25 10:47:38 -0400

    added a function to changed the state of the card from flipped, to unflipped and vice versa. the state is then used to change the class of the card to flip it.

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 import './MemoryCard.css'
 
 class MemoryCard extends Component {
+  constructor(props) {
+    super(props); 
+    this.state = { isFlipped: false, };
+  }
+  
+  clickHandler = () => {
+    this.setState({isFlipped: !this.state.isFlipped})
+  }
+  
   render() {
+    let MemoryCardInnerClass = "MemoryCardInner";
+    this.state.isFlipped === true ? MemoryCardInnerClass = "MemoryCardInner flipped" : MemoryCardInnerClass = "MemoryCardInner";
     return (
-      <div className="MemoryCard">
-        <img 
-        src='https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png' 
-        alt='digital crafts logo' 
-        />
+      <div className="MemoryCard" onClick={this.clickHandler}>
+        <div className={MemoryCardInnerClass}>
+          <div className="MemoryCardBack">
+            <img 
+            src='https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png' 
+            alt='digital crafts logo' 
+            />
+          </div>
+          <div className="MemoryCardFront">
+            ∆
+          </div>
+        </div>
       </div>
     );
   };
